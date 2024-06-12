87b606d93499a9bdfa4d0c3d9aff2b1c8598b294
Syed Nabeel Sharafat <nabeelsharafat@gmail.com>
2024-06-12 14:26:47 +0500

    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import { Hero } from "./_sections/hero";
 
 export default function Home() {
   return (
-      <div>Page</div>
+    <>
+      <Hero />
+    </>
   );
 }
